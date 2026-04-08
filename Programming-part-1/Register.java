3caec2c93b34ac23b8e3ed3d7aab091eb4f96d3e
Mbali Ntokozo Nkala <166023505+Ntokozo-Nkala@users.noreply.github.com>
2026-04-08 13:02:39 +0200

    created  login main, register, and welcome files.

---
+++ Programming-part-1/Register.java
@@ -0,0 +1,12 @@
+public class Register {
+    private String firstName;
+    private String lastName; 
+    private String userName;
+    private String password;
+    private String cellPhoneNumber;
+
+    static boolean checkUserName(String userName){
+    
+    }
+
+}
