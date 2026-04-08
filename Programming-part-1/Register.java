656ef0f332e47c417c5ab2c9144d5b09f5ff45fe
Mbali Ntokozo Nkala <166023505+Ntokozo-Nkala@users.noreply.github.com>
2026-04-08 11:51:34 +0000

    created register logic

--- Programming-part-1/Register.java
+++ Programming-part-1/Register.java
@@ -5,8 +5,27 @@ public class Register {
     private String password;
     private String cellPhoneNumber;
 
-    static boolean checkUserName(String userName){
+    public boolean checkUserName(String userName){
+    boolean hasUnderscore = userName.contains("_");
+    boolean shortEnough = userName.length() <= 5;
+    return hasUnderscore && shortEnough; 
+    }
+
+    public boolean checkUserPasswordComplexity(String password){
+        boolean shortEnough = password.length() < 8;
+        boolean hasCapital = true;
+        booolean hasNumber = true;
+        boolean hasSpecialCharacter = true;
+        return shortEnough && hasCapital && hasNmber && hasSpecialCharacter;
+    }
     
+    public boolean checkCellPhoneNumber(String cellPhoneNumber){
+        
+    }
+
+
+    public String registerUser(String userName, String password, String cellPhoneNUmber){
+
     }
 
 }
