dd0fe1ae92a54f9b2153ec345d2741a9006e7e9b
Mbali Ntokozo Nkala <166023505+Ntokozo-Nkala@users.noreply.github.com>
2026-04-10 19:58:33 +0200

    Implement user registration and login methods
    Added methods to check username, password complexity, and cell phone number. Implemented user registration and login functionality.

--- Programming-part-1/Main.java
+++ Programming-part-1/Main.java
@@ -53,13 +53,30 @@ System.out.println("Cell Phone Number: " + cellPhoneNumber);
   System.out.println("Enter your password: ");
   password = scanner.nextLine();
 }
-
+    
+    ///////////////////////////////////////////////////////
+    // THIS checkUserName METHOD CHECKS THE USER NAME    //  
+    // THE USERNAME MUST CONSIST OF:                     //
+    // *AN UNDERSCORE                                    //
+    // * AND SHOULD HAVE 5 OR LESS LETTERS               //
+    //  IT RETURNS TRUE IF BOTH CONDITIONS ARE MET       //
+    ///////////////////////////////////////////////////////
 public boolean checkUserName(String Name){
 boolean hasUnderscore = userName.contains("_");
 boolean shortEnough = userName.length() <= 5;
 return hasUnderscore && shortEnough;
 }
 
+
+   /////////////////////////////////////////////////////////////////////////////////
+    // THIS checkUserPasswordComplexity METHOD CHECKS THE USERPASSWORD COMPLEXITY //  
+    // THE PASSWORD MUST CONSIST OF:                                             //
+    // *AT LEAST EIGHT CHARACTERS LONG                                          //
+    // *CONTAINS A CAPITAL LETTER                                              //
+    // *CONTAINS A NUMBER                                                     //
+    // *CONTAINS A SPECIAL CHARACTER                                         //
+    //  IT RETURNS TRUE IF ALL 4 CONDITIONS ARE MET                         //
+    /////////////////////////////////////////////////////////////////////////
 public boolean checkUserPasswordComplexity(String password){
 boolean shortEnough = password.length() < 8;
 boolean hasCapital = false;
@@ -68,6 +85,20 @@ boolean hasSpecialCharacter = false;
 return shortEnough && hasCapital && hasNumber && hasSpecialCharacter;
 }
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // THIS checkCellPhoneNumber METHOD CHECKS THE FOLLOWING:                                                                      //  
+    // THE PHONE NUMBER MUST CONSIST OF:                                                                                          //
+    // *A SOUTH AFRICAN INTERNATIONAL CODE WHICH IS +27                                                                          //
+    // *THEN THE INTERNATIONAL CODE MUAT BE FOLLOWED BY ANY NUMBERS FROM 0-9                                                    //
+    // *THEN NOT MORE THAN 10 CHARACTERS LONG                                                                                  //
+    // *IT RETURNS TRUE IF THE 2 CONDITIONS ARE MET                                                                           //
+    // *IT THEN REGISTERS THE USER SUCCESSFULLY IF ALL CONDITIONS ARE MET                                                    //
+    // *IT SAVES ALL THE REGISTRATION INFORMATION SO THAT WE CAN USE THE INFORMATION IN THE LOGIN                           //
+    //  THE REGEX PATTERN IS FROM:                                                                                         //
+    //https://www.geeksforgeeks.org/java/java-program-to-check-for-a-valid-mobile-number/                                 //
+    //https://www.geeksforgeeks.org/dsa/validate-phone-numbers-with-country-code-extension-using-regular-expression/     //
+    //https://www.geeksforgeeks.org/dsa/write-regular-expressions/                                                      //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public boolean checkCellPhoneNumber(String cellPhoneNumber){
 String phoneNumberRegex = "^\\+27[0-9]{9}$";
 if(cellPhoneNumber.matches(phoneNumberRegex)){
@@ -76,6 +107,14 @@ if(cellPhoneNumber.matches(phoneNumberRegex)){
 return cellPhoneNumber.matches(phoneNumberRegex);
 }
 
+
+  //////////////////////////////////////////////////////////////////////////////////////////// 
+    // THIS registerUser METHOD CHECKS ALL THE USERS INFORMATION                             //  
+    // IF ALL THE CONDITIONS ARE MET THE THE USER WILL BE SUCCESSFULLY REGISTERED           //
+    // *A MESSAGE WILL BE DISPLAYED THAT THE REGISTRATION IS SUCCESSFUL                    //
+    // *IF ANY CONDITION IS NOT MET, AN ERROR TEXT OF THAT FIELD WILL BE DISPLAYED        //
+    //  *IF THE ENTERED IFNORMATION IS CORRECT THEN IT WILL BE SAVED                     //
+    //////////////////////////////////////////////////////////////////////////////////////
 public String registerUser(String firstName, String lastName, String userName, String password, String cellPhoneNumber){
   if(!checkUserName(userName)){
     System.out.println("Username is not correctly formatted; please ensure that your usernamecontains an underscore and is no more than five characters in length.");
@@ -96,7 +135,14 @@ if(!checkUserPasswordComplexity(password)){
 
   return "Your Accound is successfully! Welcome," + firstName +"."; 
 }
- 
+  
+    ////////////////////////////////////////////////////////////////////////////////////
+    // THIS loginUser METHOD USES THE SAME USERNAME AND PASSWORD TO LOGIN            //  
+    // IT VERIFIES THE USER'S                                                       //
+    // IT CHECKS IF:                                                               //
+    // *ENTERED USERNAME AND PASSWORD ENTERED MATCHES THE SAVED                   //                       
+    // *IF BOTH CONDITIONNS ARE MET THE USER CAN LOG IN THEN SEE THE WELCOME PAGE//
+    //////////////////////////////////////////////////////////////////////////////
 public boolean loginUser(String userName, String password){
   
   if(this.userName == null) return false;
@@ -104,7 +150,10 @@ public boolean loginUser(String userName, String password){
 
 }
 
-
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // THIS returnLoginStatus METHOD USES THE INFORMATION IN loginUser TO RETURN THE APPROPRIATE LOG IN STATUS // 
+    // *IF BOTH CONDITIONNS ARE MET THE USER CAN LOG IN THEN SEE THE WELCOME PAGE                             //
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 public String returnLoginStatus(String userName, String password){
   if(loginUser(userName, password)){
     return"Welcome," + firstName + lastName + "." + "It is nice to see you again.";
