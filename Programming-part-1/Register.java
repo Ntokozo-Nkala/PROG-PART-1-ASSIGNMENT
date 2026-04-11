cc477b334ee341b4872ed01b44a234d2dafad598
Mbali Ntokozo Nkala <166023505+Ntokozo-Nkala@users.noreply.github.com>
2026-04-11 22:11:08 +0200

    Refactor Register class into Login with user registration

--- Programming-part-1/Register.java
+++ Programming-part-1/Register.java
@@ -1,10 +1,16 @@
-public class Register {
-    private String firstName;
-    private String lastName; 
-    private String username;
-    private String password;
-    private String cellPhoneNumber;
-    
+package com.mycompany.nkala.prog.part.assignment;
+
+import java.util.Scanner;
+
+  public class Login{
+  public static void main(String[] args){
+   String firstName;
+   String lastName;
+   String username;
+   String password;
+   String cellPhoneNumber;
+
+
     System.out.println("++==================================================++");
     System.out.println("++                                                  ++");
     System.out.println("++                 HI THERE!  ^_^                   ++");
@@ -12,39 +18,72 @@ public class Register {
     System.out.println("++                                                  ++");
     System.out.println("++                                                  ++");
     System.out.println("++==================================================++");
-    System.out.println("[]+++++++++++Account Registration++++++++++++[]");
+    System.out.println("===================Account Registration===============");
 
-public static void main(String[] args){
-      Scanner inputDevice = new Scanner(System.in);
-      System.out.print("Enter your First name: ");
-      firstName = inputDevice.nextLine();
 
-      System.out.print("Enter your Last name: ");
-      lastName = inputDevice.nextLine();
+Scanner scanner = new Scanner(System.in);
+Login userLogin = new Login();
+System.out.println("Enter your First name: ");
+firstName = scanner.nextLine();
 
-      System.out.print("Enter your Username: ");
-      username = inputDevice.nextLine();
+System.out.println("Enter your Last name: ");
+lastName = scanner.nextLine();
 
-      System.out.print("Enter your Password: ");
-      password = inputDevice.nextLine();
+System.out.println("Enter a Username: ");
+username = scanner.nextLine();
 
-      System.out.print("Enter South African Number: ");
-      cellPhoneNumber = inputDevice.nextLine();
+System.out.println("Enter a password: ");
+password = scanner.nextLine();
 
+System.out.println("Enter your Cell phone number: ");
+cellPhoneNumber = scanner.nextLine();
+
+System.out.println(userLogin.registerUser(firstName, lastName, username, password, cellPhoneNumber));
+
+if(userLogin.username == null){
+    System.out.println("Registration failed. Please try again");
+    scanner.close();
+    return;
+}
+    System.out.println("++=======================================================++");
+    System.out.println("++                                                       ++");
+    System.out.println("++  You have successfully registered your account.       ++");
+    System.out.println("++      You can now log in to your account. ^_^          ++");
+    System.out.println("++                                                       ++");
+    System.out.println("++                                                       ++");
+    System.out.println("++=======================================================++");
+    System.out.println("=====================ACCOUNT LOGIN.=======================");
+  System.out.println("Enter your Username: ");
+  username = scanner.nextLine();
+  System.out.println("Enter your Password: ");
+  password = scanner.nextLine();
+  System.out.println(userLogin.returnLoginStatus(username, password));
+  scanner.close();
+  }
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+    
+    
     ///////////////////////////////////////////////////////
     // THIS checkUserName METHOD CHECKS THE USER NAME    //  
     // THE USERNAME MUST CONSIST OF:                     //
     // *AN UNDERSCORE                                    //
     // * AND SHOULD HAVE 5 OR LESS LETTERS               //
     //  IT RETURNS TRUE IF BOTH CONDITIONS ARE MET       //
-    ///////////////////////////////////////////////////////
-    public boolean checkUserName(String username){
-    boolean hasUnderscore = userName.contains("_");
-    boolean shortEnough = userName.length() <= 5;
-    return hasUnderscore && shortEnough; // can also be return;
-    }
+    //////////////////////////////////////////////////////
+    /// @param username
+    /// @return /
+public boolean checkUserName(String username){
+boolean hasUnderscore = username.contains("_");
+boolean shortEnough = username.length() <= 5;
+return hasUnderscore && shortEnough;
+}
+    private String firstName;
 
-    /////////////////////////////////////////////////////////////////////////////////
+
+   /////////////////////////////////////////////////////////////////////////////////
     // THIS checkUserPasswordComplexity METHOD CHECKS THE USERPASSWORD COMPLEXITY //  
     // THE PASSWORD MUST CONSIST OF:                                             //
     // *AT LEAST EIGHT CHARACTERS LONG                                          //
@@ -52,15 +91,22 @@ public static void main(String[] args){
     // *CONTAINS A NUMBER                                                     //
     // *CONTAINS A SPECIAL CHARACTER                                         //
     //  IT RETURNS TRUE IF ALL 4 CONDITIONS ARE MET                         //
-    /////////////////////////////////////////////////////////////////////////
-    public boolean checkUserPasswordComplexity(String password){
-        boolean shortEnough = password.length() < 8;
-        boolean hasCapital = false;
-        booolean hasNumber = false;
-        boolean hasSpecialCharacter = false;
-        return shortEnough && hasCapital && hasNmber && hasSpecialCharacter;
-    }
-    
+    ////////////////////////////////////////////////////////////////////////
+    /// @param password/
+    /// @return 
+public boolean checkUserPasswordComplexity(String password){
+boolean longEnough = password.length()>= 8;
+boolean hasUpper = false;
+boolean hasDigit = false;
+boolean hasSpecial = false;
+
+for(char c : password.toCharArray()){
+    if (Character.isUpperCase(c))        hasUpper   = true;
+    if (Character.isDigit(c))            hasDigit   = true;
+    if (!Character.isLetterOrDigit(c))   hasSpecial = true;
+}
+return hasUpper && hasDigit && hasSpecial;
+}
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // THIS checkCellPhoneNumber METHOD CHECKS THE FOLLOWING:                                                                      //  
@@ -75,10 +121,13 @@ public static void main(String[] args){
     //https://www.geeksforgeeks.org/java/java-program-to-check-for-a-valid-mobile-number/                                 //
     //https://www.geeksforgeeks.org/dsa/validate-phone-numbers-with-country-code-extension-using-regular-expression/     //
     //https://www.geeksforgeeks.org/dsa/write-regular-expressions/                                                      //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public boolean checkCellPhoneNumber(String phoneNumber){
-    return phoneNumber.matches = "^\\+27[0-9]{9}$";
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @param cellPhoneNumber/
+    /// @return 
+public boolean checkCellPhoneNumber(String cellPhoneNumber){
+String phoneNumberRegex = "^\\+27[0-9]{9}$";
+return cellPhoneNumber.matches(phoneNumberRegex);
+}
 
 
     //////////////////////////////////////////////////////////////////////////////////////////// 
@@ -87,67 +136,69 @@ public static void main(String[] args){
     // *A MESSAGE WILL BE DISPLAYED THAT THE REGISTRATION IS SUCCESSFUL                    //
     // *IF ANY CONDITION IS NOT MET, AN ERROR TEXT OF THAT FIELD WILL BE DISPLAYED        //
     //  *IF THE ENTERED IFNORMATION IS CORRECT THEN IT WILL BE SAVED                     //
-    //////////////////////////////////////////////////////////////////////////////////////
-    public String registerUser(String firstName, String lastName, String username, String password, String phoneNumber){
-    if(registerUser(username.equals(checkUserName) && password.equals(checkUserPasswordComplexity))){
-        return "Hi" + "," + firstName + "Your account has been registered successfully!";
-    }
-    else if(!checkUserName(username)){
-        return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length";
-    }
-    else if(!checkPasswordComplexity(password)){
-        return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number and a special character.";
-    }
-    if(phoneNumber){
-        return "Cell phone number successfully added.";
-    }
-    else{
-        return "Cell phone number incorrectly formatted or does not contain international code";
+    /////////////////////////////////////////////////////////////////////////////////////
+    /// @param firstName
+    /// @param lastName
+    /// @param username
+    /// @param password
+    /// @param cellPhoneNumber
+    /// @return /
+public String registerUser(String firstName, String lastName, String username, String password, String cellPhoneNumber){
+  if(!checkUserName(username)){
+   return "Username is not correctly formatted; please ensure that your usernamecontains an underscore and is no more than five characters in length.";
+  }
+
+if(!checkUserPasswordComplexity(password)){
+   return"Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+}
+    if(!checkCellPhoneNumber(cellPhoneNumber)){
+       return "Cell phone number incorrectly formatted or does not contains international code.";
     }
 
-    //the register now saves the information and used in the login. 
     this.firstName = firstName;
     this.lastName = lastName;
     this.username = username;
     this.password = password;
-    this.cellPhoneNumber = phoneNumber;
+    this.cellPhoneNumber = cellPhoneNumber;
 
-}    
-
+  return "Your Account is successfully registered! Welcome," + firstName +"."; 
+}
+  
     ////////////////////////////////////////////////////////////////////////////////////
     // THIS loginUser METHOD USES THE SAME USERNAME AND PASSWORD TO LOGIN            //  
     // IT VERIFIES THE USER'S                                                       //
     // IT CHECKS IF:                                                               //
-    // *ENTERED USERNAME AND PASSWORD ENTERED MATCHES THE SAVED                   //                         //
+    // *ENTERED USERNAME AND PASSWORD ENTERED MATCHES THE SAVED                   //                       
     // *IF BOTH CONDITIONNS ARE MET THE USER CAN LOG IN THEN SEE THE WELCOME PAGE//
-    //////////////////////////////////////////////////////////////////////////////
-    public boolean loginUser(String username, String password){
-        if(this.username.equals(username) && this.password.equals(password)){
-            return true;
-        }
-        else{
-            return false;
-        }
-   }
+    /////////////////////////////////////////////////////////////////////////////
+    /// @param userName/
+    /// @param password
+    /// @return 
+ public boolean loginUser(String userName, String password){
+  
+  if(this.username == null) return false;
+  return this.username.equals(userName) && this.password.equals(password);
+
+}
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // THIS returnLoginStatus METHOD USES THE INFORMATION IN loginUser TO RETURN THE APPROPRIATE LOG IN STATUS // 
     // *IF BOTH CONDITIONNS ARE MET THE USER CAN LOG IN THEN SEE THE WELCOME PAGE                             //
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////  
-   public String returnLoginStatus(String username, String password){
-    if(loginUser.equals(username) && loginUser.equals(password)){
-        return "Welcome back" + ","  + firstName + lastName + "it is great to see you!";
-    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ ///
+ ////// @param userName/
+    /// @param password
+    /// @return 
+public String returnLoginStatus(String userName, String password){
+  if(loginUser(userName, password)){
+    return"Welcome" + firstName + "," + lastName + "." + "It is nice to see you again.";
+  }
     else{
-        return "Login failed, please ensure that the Username amd Password are correctly formatted";
+      return"Username or password incorrect. please try again.";
     }
+  }
+
+    
 }
-    public String getFirstName(){
-        return firstName;
-    }
-    public String getLastName(){
-        return lastName;
-    }
-}
-}
-}
+
